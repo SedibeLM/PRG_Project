ebda93af9277e0ba87f5a202246620e89b983ad5
Lesedi Mmathapelo Sedibe <578471@student.belgiumcampus.ac.za>
2024-11-10 20:07:25 +0200

    Found missing files

---
+++ UpdateStudentForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace PRG_Project
+{
+    partial class UpdateStudentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(286, 275);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(208, 56);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // UpdateStudentForm
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 398);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "UpdateStudentForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
---
+++ UpdateStudentForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace PRG_Project
+{
+    public partial class UpdateStudentForm : Form
+    {
+        public UpdateStudentForm()
+        {
+            InitializeComponent();
+        }
+
+        // Method to search and display student details for editing
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string studentID = txtStudentID.Text;
+            var students = File.ReadAllLines("students.txt");
+
+            foreach (var line in students)
+            {
+                var fields = line.Split(',');
+                if (fields[0] == studentID)
+                {
+                    // Populate input fields with student data
+                    txtName.Text = fields[1];
+                    txtAge.Text = fields[2];
+                    txtCourse.Text = fields[3];
+                    return;
+                }
+            }
+
+            MessageBox.Show("Student not found.");
+        }
+
+        // Method to save updated student information back to file
+        private void btnSaveUpdate_Click(object sender, EventArgs e)
+        {
+            string studentID = txtStudentID.Text;
+            string[] lines = File.ReadAllLines("students.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] fields = lines[i].Split(',');
+
+                // Find and update student record
+                if (fields[0] == studentID)
+                {
+                    fields[1] = txtName.Text;
+                    fields[2] = txtAge.Text;
+                    fields[3] = txtCourse.Text;
+                    lines[i] = string.Join(",", fields);
+
+                    // Write updated data to file
+                    File.WriteAllLines("students.txt", lines);
+                    MessageBox.Show("Student updated successfully.");
+                    return;
+                }
+            }
+
+            MessageBox.Show("Error updating student.");
+        }
+    }
+}
+
