804d5e30c66a346f3287d3d4a875a1cb97228f24
Lesedi Mmathapelo Sedibe <578471@student.belgiumcampus.ac.za>
2024-11-10 22:53:02 +0200

    Fixes made in thne main form

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,10 +30,14 @@
         {
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button5
@@ -44,7 +48,6 @@
             this.button5.TabIndex = 1;
             this.button5.Text = "Clear";
             this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -55,46 +58,81 @@
             this.button6.Text = "Save";
             this.button6.UseVisualStyleBackColor = true;
             // 
-            // textBox5
+            // txtStudentID
             // 
-            this.textBox5.Location = new System.Drawing.Point(48, 30);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(127, 26);
-            this.textBox5.TabIndex = 3;
-            this.textBox5.Text = "Student ID";
-            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            this.txtStudentID.Location = new System.Drawing.Point(303, 30);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(127, 26);
+            this.txtStudentID.TabIndex = 3;
             // 
-            // textBox6
+            // txtName
             // 
-            this.textBox6.Location = new System.Drawing.Point(48, 92);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(127, 26);
-            this.textBox6.TabIndex = 4;
-            this.textBox6.Text = "Name";
+            this.txtName.Location = new System.Drawing.Point(303, 84);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(127, 26);
+            this.txtName.TabIndex = 4;
             // 
-            // textBox7
+            // txtAge
             // 
-            this.textBox7.Location = new System.Drawing.Point(48, 151);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(127, 26);
-            this.textBox7.TabIndex = 5;
-            this.textBox7.Text = "Age";
+            this.txtAge.Location = new System.Drawing.Point(303, 149);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(127, 26);
+            this.txtAge.TabIndex = 5;
             // 
-            // textBox8
+            // txtCourse
             // 
-            this.textBox8.Location = new System.Drawing.Point(48, 209);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(127, 26);
-            this.textBox8.TabIndex = 6;
-            this.textBox8.Text = "Course";
+            this.txtCourse.Location = new System.Drawing.Point(303, 206);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(127, 26);
+            this.txtCourse.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Student ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(120, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Age";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(120, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Course";
             // 
             // MainForm
             // 
             this.ClientSize = new System.Drawing.Size(1127, 459);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtStudentID);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Name = "MainForm";
@@ -114,10 +152,14 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
