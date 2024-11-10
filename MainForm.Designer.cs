3a104165e981ac83b89bf30476f85d3d1438a6c6
Lesedi Mmathapelo Sedibe <578471@student.belgiumcampus.ac.za>
2024-11-10 22:55:34 +0200

    Fixes made in the main form

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -37,7 +37,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lAbel4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button5
@@ -113,19 +113,19 @@
             this.label3.TabIndex = 9;
             this.label3.Text = "Age";
             // 
-            // label4
+            // lAbel4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(120, 212);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 20);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Course";
+            this.lAbel4.AutoSize = true;
+            this.lAbel4.Location = new System.Drawing.Point(120, 212);
+            this.lAbel4.Name = "lAbel4";
+            this.lAbel4.Size = new System.Drawing.Size(60, 20);
+            this.lAbel4.TabIndex = 10;
+            this.lAbel4.Text = "Course";
             // 
             // MainForm
             // 
             this.ClientSize = new System.Drawing.Size(1127, 459);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lAbel4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -159,7 +159,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lAbel4;
     }
 }
 
